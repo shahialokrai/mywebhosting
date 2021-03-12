042cc159b48e3bf1fa02e718b157f6657f0ca592
shahialokrai <shahialokrai@gmail.com>
2021-03-12 09:43:58 +0530

    changed

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const AuthController = require('../controllers/AuthController')
+const AuthController = require('../controllers/Authcontroller')
 
 router.post('/register', AuthController.register)
 router.post('/login', AuthController.login)
